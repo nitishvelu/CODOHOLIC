4af7ad813674621b26f50206cb030162ed5400b0
Srinivas-VJ <srinivasvj01@gmail.com>
2020-10-24 21:17:30 +0530

    Removed unnessacary steps

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App theme={{
-      color: 'green'
-    }}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
