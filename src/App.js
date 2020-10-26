1c9eb9f73dd7f540b44704a7784fbe7d5781eebc
Srinivas-VJ <srinivasvj01@gmail.com>
2020-10-26 16:41:35 +0530

    removed unwanted stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
   return (
     <div className="App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{
-      backgroundColor: props.theme.color
-    }}>
-          This Website is a scam.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!-- components go here--> */}
     </div>
   );
 }
