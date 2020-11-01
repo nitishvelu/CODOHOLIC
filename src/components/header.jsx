6d7286126db8b0131547ae90ca47fbae1f1cc75c
Srinivas-VJ <srinivasvj01@gmail.com>
2020-11-01 19:17:13 +0530

    new

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
 import NavBar from './Navbar';
-import logo from '../images/coding.svg';
-const stylesheet = {
-    logo: {
-        height: 36,
-        margin: '0 20px',
-    },
-    title: {
-        fontFamily: 'monospace',
-        fontSize: 25,
-    },
-    header: {
-        backgroundColor: 'yellow',
-        top: 0,
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    }
-}
-
 
 class Header extends React.Component {
     // constructor ()
@@ -28,25 +8,12 @@ class Header extends React.Component {
     //     super()
     // }
 
-
-
     render()
     {
         return (
-            <div >
-            <header style = {stylesheet.header}>
-                <div style={{flexDirection:'row', display: "flex", alignItems: 'center'}}>
-                    <img 
-                    style = {stylesheet.logo}
-                    id = 'logo' 
-                    src={logo} 
-                    alt="logo"
-                    />
-                    <h1 id = 'title' style = {stylesheet.title}>Codoholic</h1>
-                </div>
+            <header>
                 <NavBar />
             </header>
-            </div>
         )
     }
 }
