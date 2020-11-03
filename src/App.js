918927348c471babb9103125fa8f580e21430aa0
Srinivas-VJ <srinivasvj01@gmail.com>
2020-11-03 23:24:06 +0530

    Fixed cursor

--- src/App.js
+++ src/App.js
@@ -11,11 +11,69 @@ function generateSampleText()
 {
   // this function has to generate sample text for now just returning text
   return 'This is the sample text that has to be typed. Take typing speed test, practice your touch typing skills, learn to type faster and with fewer errors with this free online typing tutor.Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do. It significantly improves typing speed and eliminates errors. Touch typing simply makes you more productive and it is a skill worth learning. However, many people engage in the bad habit of ‘hunt-and-peck’ typing, even those seasoned professionals with years and decades of computer experience. It  is easy to understand, though, as touch typing requires dedicated practice to  learn it well.'
-
 }
 
-
 sampleText = generateSampleText();
+
+
+class Controller extends React.Component {
+  constructor(props)
+  {
+    //useless constructor will add stuff later
+    super(props);
+    this.state = {
+      sampleIndex : 0,
+    };
+    
+  }
+
+  shiftCursor = pos =>
+  {
+      let division = document.getElementById('sampBox');
+      division.children[pos].classList.add('activeSpan');
+      if (this.state.sampleIndex  !== 0)
+      {
+        division.children[pos - 1].classList.remove('activeSpan');
+        division.children[pos - 1].classList.add('typedSpan');
+      }
+
+  }
+
+  handleKey = event =>
+  {
+    if(event.key === this.props.sampleText[this.state.sampleIndex])
+    {
+      
+      this.setState({
+        sampleIndex : this.state.sampleIndex + 1,
+      });
+      if (this.state.sampleIndex === 0)
+      {
+        document.getElementById('sampBox').children[0].classList.remove('activeSpan');
+        document.getElementById('sampBox').children[0].classList.add('typedSpan');
+      }
+
+      this.shiftCursor(this.state.sampleIndex + 1);
+      
+    }
+  }
+
+  clickHandler = event => {
+    document.getElementById('sampBox').children[0].classList.add('activeSpan');
+    event.target.style.display = 'none';
+  }
+
+
+   render()
+    {
+      return (
+      <div onKeyPress={this.handleKey} tabIndex = '0' onClick={this.clickHandler} style = {{ fontSize : 22, backgroundColor: '#282828' }}>
+        <center>Click to start</center>
+      </div>
+      );
+    }
+}
+
 
 
 function App(props) {
@@ -29,9 +87,11 @@ function App(props) {
 
       <Footer />
 
-
     </div>
   );
 }
 
-export default App;
+export {
+  App,
+  Controller
+};
--- src/components/SampleTextBox.jsx
+++ src/components/SampleTextBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SampleTextBox.css';
-
+import {Controller} from '../App';
 
 var style = {
   text : {
@@ -40,66 +40,6 @@ class SampleTextBox extends React.Component {
       </div>
     );
   }
-}
-
-
-class Controller extends React.Component {
-  constructor(props)
-  {
-    //useless constructor will add stuff later
-    super(props);
-    this.state = {
-      sampleIndex : 0,
-    };
-    
-  }
-
-  shiftCursor = pos =>
-  {
-      let division = document.getElementById('sampBox');
-      division.children[pos].classList.add('activeSpan');
-      if (this.state.sampleIndex  !== 0)
-      {
-        division.children[pos - 1].classList.remove('activeSpan');
-        division.children[pos - 1].classList.add('typedSpan');
-      }
-
-  }
-
-  handleKey = event =>
-  {
-    console.log(`type - ${this.props.sampleText[this.state.sampleIndex + 1]}`);
-    if(event.key === this.props.sampleText[this.state.sampleIndex])
-    {
-      
-      this.setState({
-        sampleIndex : this.state.sampleIndex + 1,
-      });
-      if (this.state.sampleIndex === 0)
-      {
-        document.getElementById('sampBox').children[0].classList.remove('activeSpan');
-        document.getElementById('sampBox').children[0].classList.add('typedSpan');
-      }
-
-      this.shiftCursor(this.state.sampleIndex + 1);
-      
-    }
-  }
-
-  clickHandler = event => {
-    document.getElementById('sampBox').children[0].classList.add('activeSpan');
-    event.target.style.display = 'none';
-  }
-
-
-   render()
-    {
-      return (
-      <div onKeyPress={this.handleKey} tabIndex = '0' onClick={this.clickHandler} style = {{ fontSize : 18, backgroundColor: '#282828' }}>
-        <center>click to activate</center>
-      </div>
-      );
-    }
 }
 
 
