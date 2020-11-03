b31400e68e108a7f185b0e7ad6a44dddc71da616
Srinivas-VJ <srinivasvj01@gmail.com>
2020-11-03 21:41:58 +0530

    Fixed slow cursor works fine

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ var sampleText;
 function generateSampleText()
 {
   // this function has to generate sample text for now just returning text
-  return 'This is the sample text that has to be typed.\n Take typing speed test, practice your touch typing skills, learn to type faster and \n with fewer errors with this free online typing tutor.Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do. It significantly improves typing speed and eliminates errors. Touch typing simply makes you more productive and it is a skill worth learning. However, many people engage in the bad habit of ‘hunt-and-peck’ typing, even those seasoned professionals with years and decades of computer experience. It  is easy to understand, though, as touch typing requires dedicated practice to  learn it well.'
+  return 'This is the sample text that has to be typed. Take typing speed test, practice your touch typing skills, learn to type faster and with fewer errors with this free online typing tutor.Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do. It significantly improves typing speed and eliminates errors. Touch typing simply makes you more productive and it is a skill worth learning. However, many people engage in the bad habit of ‘hunt-and-peck’ typing, even those seasoned professionals with years and decades of computer experience. It  is easy to understand, though, as touch typing requires dedicated practice to  learn it well.'
 
 }
 
--- src/components/SampleTextBox.jsx
+++ src/components/SampleTextBox.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import './SampleTextBox.css'
+import './SampleTextBox.css';
+
+
 var style = {
   text : {
-    // padding : 0.05,
+    padding : 1,
     fontSize : 28
     },
   block : {
@@ -12,13 +14,10 @@ var style = {
     border: 'solid 11px #ffff01ed',
     color: 'white',
   }
-
 }
 
 
 class SampleTextBox extends React.Component {
-  
-  
   constructor(props)
   {
     //useless constructor will add stuff later
@@ -29,41 +28,79 @@ class SampleTextBox extends React.Component {
     this.arr = this.props.sampleText.split('');
     this.arr = this.arr.map((ele, index) => {
       return (<span style={style.text} key={index} className='sampleText'>{ele}</span>)
-    });
-  }
-  compute
- 
-  handleKey = event =>
-  {
-    //this.addHighlight(this.arr[this.state.sampleIndex])
-    if(event.key === this.props.sampleText[this.state.sampleIndex])
-    {
-          this.arr = this.arr.map((ele, index) => {
-            return (<span style={style.text} key={index} className={`sampleText ${index<(this.state.sampleIndex + 1)? 'typedSpan': ''}`}>{ele}</span>)
-          });
-      this.setState( (prevState) => {
-        return {
-          sampleIndex : prevState.sampleIndex + 1
-        }
       });
-    }
-    // console.log(this.state.sampleIndex)
-
   }
- 
-  
 
   render ()
   {
-    
-    
-
-
     return (
-      <div style={style.block} onKeyPress={this.handleKey} tabIndex = '0'>
-      {this.arr}
+      <div style = {style.block} id = 'sampBox' >
+        {this.arr}
+        <Controller sampleText = {this.props.sampleText} / > 
       </div>
-    )
+    );
   }
 }
+
+
+class Controller extends React.Component {
+  constructor(props)
+  {
+    //useless constructor will add stuff later
+    super(props);
+    this.state = {
+      sampleIndex : 0,
+    };
+    
+  }
+
+  shiftCursor = pos =>
+  {
+      let division = document.getElementById('sampBox');
+      division.children[pos].classList.add('activeSpan');
+      if (this.state.sampleIndex  !== 0)
+      {
+        division.children[pos - 1].classList.remove('activeSpan');
+        division.children[pos - 1].classList.add('typedSpan');
+      }
+
+  }
+
+  handleKey = event =>
+  {
+    console.log(`type - ${this.props.sampleText[this.state.sampleIndex + 1]}`);
+    if(event.key === this.props.sampleText[this.state.sampleIndex])
+    {
+      
+      this.setState({
+        sampleIndex : this.state.sampleIndex + 1,
+      });
+      if (this.state.sampleIndex === 0)
+      {
+        document.getElementById('sampBox').children[0].classList.remove('activeSpan');
+        document.getElementById('sampBox').children[0].classList.add('typedSpan');
+      }
+
+      this.shiftCursor(this.state.sampleIndex + 1);
+      
+    }
+  }
+
+  clickHandler = event => {
+    document.getElementById('sampBox').children[0].classList.add('activeSpan');
+    event.target.style.display = 'none';
+  }
+
+
+   render()
+    {
+      return (
+      <div onKeyPress={this.handleKey} tabIndex = '0' onClick={this.clickHandler} style = {{ fontSize : 18, backgroundColor: '#282828' }}>
+        <center>click to activate</center>
+      </div>
+      );
+    }
+}
+
+
 export default SampleTextBox;
