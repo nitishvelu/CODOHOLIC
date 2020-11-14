bf283760fd9d712387e16fc5cf63314a5709e7c6
Srinivas-VJ <srinivasvj01@gmail.com>
2020-11-15 00:11:29 +0530

    Added speed testing [only on console]

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ var sampleText;
 function generateSampleText()
 {
   // this function has to generate sample text for now just returning text
-  return 'This is the sample text that has to be typed. Take typing speed test, practice your touch typing skills, learn to type faster and with fewer errors with this free online typing tutor.Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do. It significantly improves typing speed and eliminates errors. Touch typing simply makes you more productive and it is a skill worth learning. However, many people engage in the bad habit of ‘hunt-and-peck’ typing, even those seasoned professionals with years and decades of computer experience. It  is easy to understand, though, as touch typing requires dedicated practice to  learn it well.'
+  const arr = ['Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do. It significantly improves typing speed and eliminates errors. Touch typing simply makes you more productive and it is a skill worth learning.','However, many people engage in the bad habit of ‘hunt-and-peck’ typing, even those seasoned professionals with years and decades of computer experience. It  is easy to understand, though, as touch typing requires dedicated practice to  learn it well.','That is why we created this website, a free online typing tutorial, to give you the most advanced learning experience and let you develop your typing skills faster.','1234567890'];
+  
+  let index = Math.random()<= 0.33 ? 0 : (Math.random()< 0.66 ? 1 : 2);
+  return arr[index];
 }
 
 sampleText = generateSampleText();
+
+var startTime;
 
 
 class Controller extends React.Component {
@@ -29,6 +34,7 @@ class Controller extends React.Component {
 
   shiftCursor = pos =>
   {
+    // shifts the cursor when text is typed
       let division = document.getElementById('sampBox');
       division.children[pos].classList.add('activeSpan');
       if (this.state.sampleIndex  !== 0)
@@ -41,9 +47,18 @@ class Controller extends React.Component {
 
   handleKey = event =>
   {
+    // handle key event on the controller component
+    if(this.state.sampleIndex === this.props.sampleText.length - 1)
+      {
+        // end of sample computing time
+        this.computeSampleSpeed();
+      }
+    
     if(event.key === this.props.sampleText[this.state.sampleIndex])
     {
-      
+      if(this.state.sampleIndex === 0)
+        this.startTimer();// starting timer
+
       this.setState({
         sampleIndex : this.state.sampleIndex + 1,
       });
@@ -58,16 +73,41 @@ class Controller extends React.Component {
     }
   }
 
-  clickHandler = event => {
+  controllerClickHandler = event => {
+    // handles click of controller
+    // activates text
     document.getElementById('sampBox').children[0].classList.add('activeSpan');
-    event.target.style.display = 'none';
+    document.getElementById('controller').children[0].innerHTML = '';
   }
 
+   controllerHandleBlur = event => {
+    // handles loosing focus
+    let i;
+    for(i = 0;i<this.state.sampleIndex;i++)
+      document.getElementById('sampBox').children[i].classList.remove('typedSpan');
+    document.getElementById('sampBox').children[i].classList.remove('activeSpan');
+
+    this.setState({
+      sampleIndex : 0,
+    });
+    document.getElementById('controller').children[0].innerHTML = 'Click to start';
+   }
+
+   startTimer = () => {startTime = new Date()} //starts timer
+
+   computeSampleSpeed = () => {
+    const elapsedTime = (new Date() - startTime) / 1000;
+    const words = (this.props.sampleText.length - 1)/5;
+    const speed = Math.round(words/(elapsedTime/60));
+    
+    console.log(`elapsed time - ${elapsedTime}\nno of words = ${words} \nspeed - ${speed}WPM`);
+
+   }
 
    render()
     {
       return (
-      <div onKeyPress={this.handleKey} tabIndex = '0' onClick={this.clickHandler} style = {{ fontSize : 22, backgroundColor: '#282828' }}>
+      <div id = 'controller' onBlur = {this.controllerHandleBlur}onKeyPress={this.handleKey} tabIndex = '0' onClick={this.controllerClickHandler} style = {{ fontSize : 22, backgroundColor: '#282828' }}>
         <center>Click to start</center>
       </div>
       );
--- src/components/SampleTextBox.jsx
+++ src/components/SampleTextBox.jsx
@@ -2,39 +2,22 @@ import React from 'react';
 import './SampleTextBox.css';
 import {Controller} from '../App';
 
-var style = {
-  text : {
-    padding : 1,
-    fontSize : 28
-    },
-  block : {
-    backgroundColor: '#000',
-    padding : 35,
-    fontFamily : 'monospace',
-    border: 'solid 11px #ffff01ed',
-    color: 'white',
-  }
-}
-
 
 class SampleTextBox extends React.Component {
   constructor(props)
   {
     //useless constructor will add stuff later
     super(props);
-    this.state = {
-      sampleIndex : 0,
-    };
     this.arr = this.props.sampleText.split('');
     this.arr = this.arr.map((ele, index) => {
-      return (<span style={style.text} key={index} className='sampleText'>{ele}</span>)
+      return (<span key={index} className='sampleText'>{ele}</span>)
       });
   }
 
   render ()
   {
     return (
-      <div style = {style.block} id = 'sampBox' >
+      <div id = 'sampBox' >
         {this.arr}
         <Controller sampleText = {this.props.sampleText} / > 
       </div>
