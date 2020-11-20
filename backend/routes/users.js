11a3e3ece903c145c5f4be04b784f92b97c3124f
nitishvelu <nitishvelu@gmail.com>
2020-11-20 21:21:35 +0530

    for push

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,7 +15,7 @@ router.post(
     (req,res)=> {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array})
+        return res.status(400).json({errors: errors.array()})
     }
     res.send('user route');
 
