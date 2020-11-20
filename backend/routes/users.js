6eec026352b4f80f4a5bd67a15d21887d37916ce
nitishvelu <nitishvelu@gmail.com>
2020-11-20 21:15:32 +0530

    password validation under users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
 const router =express.Router();
+const { check, validateResult, validationResult } =require('express-validator/check');
+//user regestration
+router.post(
+    '/',
+    [
+        check('name','Name is required')
+        .not()
+        .isEmpty(),
+        check('email','please include a valid email')
+        .isEmail(),
+        check('password','please enter a password with 6 or more characters').isLength({min: 6})
+    ],
+    (req,res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array})
+    }
+    res.send('user route');
 
-router.get('/',(req,res)=> res.send('user route'));
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 connectDB(); //connecting to Database
+
+//initializing middleware
+app.use(express.json({extended: false}));
 app.get('/',(req,res)=> res.send('API running'));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => console.log('server started on port ${PORT} ') );
+//define routes
+app.use('/users',require('./routes/users'));
+app.use('/profile',require('./routes/profile'));
+app.use('/auth',require('./routes/auth'));
+
+
+app.listen(PORT,() => console.log('server started on port ${PORT}') );
