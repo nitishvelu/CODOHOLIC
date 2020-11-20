093d0dcd0622cb3f87965f536551d4c97b744766
nitishvelu <nitishvelu@gmail.com>
2020-11-20 18:33:17 +0530

    connected to MongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+connectDB(); //connecting to Database
 app.get('/',(req,res)=> res.send('API running'));
 const PORT = process.env.PORT || 5000;
 
