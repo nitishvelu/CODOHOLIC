e9abe1625b9e8c53290aa960b175ad48e8f22cb8
nitishvelu <nitishvelu@gmail.com>
2020-11-21 21:05:47 +0530

    routing through auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const router =express.Router();
+const auth =require('../middleware/auth');
 
-router.get('/',(req,res)=> res.send('Auth route'));
+router.get('/',auth,(req,res)=> res.send('Auth route'));
 
 module.exports = router;
