9a275c90e94841ab1303ba91e66c86b2b09d95c0
nitishvelu <nitishvelu@gmail.com>
2020-11-21 21:21:43 +0530

    retrieving user info with tokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const router =express.Router();
 const auth =require('../middleware/auth');
+const User =require('../models/User');
 
-router.get('/',auth,(req,res)=> res.send('Auth route'));
+router.get('/',auth,async (req,res)=> {
+    try{
+        const user= await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+});
 
 module.exports = router;
