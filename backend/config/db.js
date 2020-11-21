99b833c5304e9b878322fcb03cf178ef788334b7
nitishvelu <nitishvelu@gmail.com>
2020-11-21 16:42:15 +0530

    fixed deprecated dependancies

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true});
+        await mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
         console.log('Mongodb connected');
     }catch(err){
         console.error(err.message);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router =express.Router();
-const { check, validateResult, validationResult } =require('express-validator/check');
+const gravatar= require('gravatar');
+const bcrypt= require('bcryptjs'); //for encrypting password on the database
+const { check, validateResult, validationResult } =require('express-validator');
 //user regestration
+const User= require('../models/User')
 router.post(
     '/',
     [
@@ -12,12 +15,47 @@ router.post(
         .isEmail(),
         check('password','please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req,res)=> {
+    async (req,res)=> {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    res.send('user route');
+    const {name, email,password}=req.body;
+    //encrypt password
+
+    //return jsonwebtoken
+    try{
+        //check if user exists
+
+        let user=await User.findOne({email });
+
+        if(user) {
+            return res.status(400).json({errors :[{msg:'User already exists'}]});
+        }
+        //get users gravatar
+        const avatar = gravatar.url(email,{
+            s: '200',
+            r:'pg',
+            d:'mm' //defaul image
+        })
+
+        user= new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt=await bcrypt.genSalt(10);
+        user.password= await bcrypt.hash(password,salt); //encrypting password
+        await user.save(); //saving to database
+        res.send('user registered');
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
 
 });
 
