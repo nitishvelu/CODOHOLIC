43040d14de5d597468cff1a36fb4285c51509f06
nitishvelu <nitishvelu@gmail.com>
2020-11-23 16:40:05 +0530

    fixed deprecated dependancies

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,12 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
+        await mongoose.connect(db,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex:true,
+            useFindAndModify: false
+        });
         console.log('Mongodb connected');
     }catch(err){
         console.error(err.message);
