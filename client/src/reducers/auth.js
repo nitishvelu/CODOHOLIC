62b0fdd3c7420c107332306d0a1a27029dea5241
nitishvelu <nitishvelu@gmail.com>
2020-12-01 22:10:07 +0530

    user loaded case auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import {
-    REGISTER_SUCCESS,REGISTER_FAIL
+    REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR
 } from '../actions/types';
 
 const initialState ={
@@ -14,6 +14,13 @@ export default function(state = initialState,action){
     const{type,payload} =action;
     
     switch(type){
+        case USER_LOADED:
+           return{
+               ...state,
+               isAuthenticated:true,
+               loading:false,
+               user:payload
+           } 
         case REGISTER_SUCCESS:
             localStorage.setItem('token',payload.token);
             return{
