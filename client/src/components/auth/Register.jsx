fe4231358f94f569838911dfab6eb92888960654
nitishvelu <nitishvelu@gmail.com>
2020-12-01 18:14:27 +0530

    testing connection between frontend and backend using axios

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React,{Fragment,useState} from 'react';
-
+import axios from 'axios';
 const Register = () => {
 
     const [formData, setFormData]= useState({
@@ -13,13 +13,31 @@ const Register = () => {
 
     const onChange = e =>setFormData({...formData,[e.target.name]: e.target.value});
 
-    const onSubmit = e =>{
+    const onSubmit =async e =>{
         e.preventDefault();
         if(password !== password2){
             console.log('passwords do not match');
 
         }else{
-            console.log(formData);
+            const newUser ={
+                name,
+                email,
+                password
+            }
+            try{
+                const config={
+                    headers: {
+                        'content-Type': 'application/json'
+                    }
+                }
+                const body =JSON.stringify(newUser);
+                const res =await axios.post('/users',body,config);
+                console.log(res.data);
+
+            }catch(err){
+                    console.error(err.response.data);
+            }
+            
         }
     }
     return (
