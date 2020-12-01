c0f991ece495f3d90be8e940a04e416658279927
nitishvelu <nitishvelu@gmail.com>
2020-12-01 20:57:45 +0530

    auth reducer

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+    REGISTER_SUCCESS,REGISTER_FAIL
+} from '../actions/types';
+
+const intialState ={
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user:null
+}
+
+export default function(state =initialState,action){
+    const{type,payload} =action;
+    
+    switch(type){
+        case REGISTER_SUCCESS:
+            localStorage.setItem('token',payload.token);
+            return{
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            }
+        case REGISTER_FAIL:
+            localStorage.removeItem('token');
+            return{
+                ...state,
+                token:null,
+                isAuthenticated:false,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
+
