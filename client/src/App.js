085b644a54936aef0c7b10f9373fba4471cce9a4
Srinivas-VJ <srinivasvj01@gmail.com>
2020-12-02 17:27:09 +0530

    modified sample text component

--- client/src/App.js
+++ client/src/App.js
@@ -13,32 +13,6 @@ import store from './store'
 import {loadUser} from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
-var sampleText;
-
-function generateSampleText()
-{
-  // this function has to generate sample text for now just returning text
-  const arr = [`Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do.`,`However, many people engage in the bad habit of hunt-and-peck typing, even those seasoned professionals with years and decades of computer experience..`,`That is why we created this website, a free online typing tutorial, to give you the most advanced learning experience and let you develop your typing skills faster.`,`body {
-    background-color: lightblue;
-  }
-  
-  h1 {
-    color: white;
-    text-align: center;
-  }
-  
-  p {
-    font-family: verdana;
-    font-size: 20px;
-  }
-  Just ChecKing if \\ is working`,`122345`];
-  //let index = Math.random()<= 0.33 ? 0 : (Math.random()< 0.66 ? 1 : 2);
-  return arr[3];
-}
-
-
-
-sampleText = generateSampleText();
 
 if (localStorage.token){
   setAuthToken(localStorage.token);
@@ -61,11 +35,7 @@ function App(props) {
     <Switch>
         <Route  path='/login' component={Login}/>
         <Route  path='/register' component={Register}/>
-        <Route
-    path='/'
-    render={(props) => (
-    <SampleTextBox {...props} sampleText={sampleText} />
-    )}/>
+        <Route path='/' component={SampleTextBox}/>
     </Switch>
     </section>
     
--- client/src/components/SampleTextBox.jsx
+++ client/src/components/SampleTextBox.jsx
@@ -9,8 +9,25 @@ class SampleTextBox extends React.Component {
   {
     //useless constructor will add stuff later
     super(props);
+
+    const arr = [`Touch typing is the ability to use muscle memory to find keys fast, without using the sense of sight, and with all the available fingers, just like piano players do.`,`However, many people engage in the bad habit of hunt-and-peck typing, even those seasoned professionals with years and decades of computer experience..`,`That is why we created this website, a free online typing tutorial, to give you the most advanced learning experience and let you develop your typing skills faster.`,`body {
+      background-color: lightblue;
+    }
     
-    this.text = this.props.sampleText.replaceAll(/(\r\n|\n|\r)/gm,"⏎");
+    h1 {
+      color: white;
+      text-align: center;
+    }
+    
+    p {
+      font-family: verdana;
+      font-size: 20px;
+    }
+    Just ChecKing if \\ is working`,`122345`];
+    //let index = Math.random()<= 0.33 ? 0 : (Math.random()< 0.66 ? 1 : 2);
+    this.sampleText = arr[3];
+    
+    this.text = this.sampleText.replaceAll(/(\r\n|\n|\r)/gm,"⏎");
     this.text = this.text.replaceAll(/\s\s\s\s/gm,"↹");
     this.arr1 = this.text.split('');
 
