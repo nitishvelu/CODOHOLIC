d27d7e5d65033b5b3c3bb4322aa8078ca28baf70
Nithis <nitishvelu@gmail.com>
2020-12-03 17:36:14 +0530

    clear profile schema

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,3 +9,4 @@ export const LOGIN_SUCCESS ='LOGIN_SUCCESS';
 export const LOGOUT='LOGOUT';
 export const GET_PROFILE='GET_PROFILE';
 export const PROFILE_ERROR='PROFILE_ERROR';
+export const CLEAR_PROFILE='CLEAR_PROFILE';
