f9eb20a841c76469006e3072b974ae2e91024e8e
Nithis <nitishvelu@gmail.com>
2020-12-03 14:12:46 +0530

    fetch profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React,{useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getCurrentProfile} from '../actions/profile';
 
-const Profile = props => {
+const Profile = ({getCurrentProfile,auth,profile}) => {
+    useEffect(()=>{
+        getCurrentProfile();
+    },[]);
     return (
         <div>
             profile
@@ -10,7 +15,13 @@ const Profile = props => {
 }
 
 Profile.propTypes = {
-
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
 }
+const mapStateToProps= state =>({
+    auth: state.auth,
+    profile: state.profile
+});
 
-export default Profile
+export default connect(mapStateToProps,{getCurrentProfile})(Profile)
