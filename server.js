a9f7982377e2e9ab9ae535c16e262b79b5d1ec8b
Srinivas-VJ <srinivasvj01@gmail.com>
2020-12-07 15:27:49 +0530

    tried to make leaderboard visible

--- server.js
+++ server.js
@@ -9,20 +9,17 @@ connectDB(); //connecting to Database
 app.use(express.json({extended: false}));
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
+    // to make it work
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
+
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
+
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
 });
 
