959a6d593e39211ecae65ed1588c0b73d6a4b02b
Nithis <nitishvelu@gmail.com>
2020-12-07 15:38:55 +0530

    mobile response

--- client/src/components/Mobile.jsx
+++ client/src/components/Mobile.jsx
@@ -3,11 +3,13 @@ import gif from '../images/mobile.gif'
 
 export const Mobile = () => {
     return (
-        <div style={{width:'200px'}}>
+        <div style={{width:'250px',padding:'20px'}}>
         <center>
         <div>
             <h1>THIS WEBSITE IS NOT FOR MOBILE USERS</h1>
+            <center>
             <img src={gif} alt="NO MOBILE"/>
+            </center>
         </div>
         </center>
         </div>
